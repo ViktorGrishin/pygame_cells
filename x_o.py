41f0c39b9472d4b2fdceb15b9842ed0c6fa08ade
ViktorGrishin <grishin06Vt@yandex.ru>
2024-02-04 22:41:54 +0700

    Домашнее задание № 2

---
+++ x_o.py
@@ -0,0 +1,116 @@
+import pygame
+
+
+class Board:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.board = [[0] * width for _ in range(height)]
+        # Знчения по умолчанию
+        self.left = 10
+        self.top = 10
+        self.cell_size = 30
+
+    # Настройка внешнего вида
+    def set_view(self, left, top, cell_size):
+        self.left = left
+        self.top = top
+        self.cell_size = cell_size
+
+    def render(self, screen):
+        for i in range(self.width):
+            for j in range(self.height):
+                # Делаем обводку
+                pygame.draw.rect(screen, 'white', (
+                    (self.left + self.cell_size * i,
+                     self.top + self.cell_size * j),
+                    (self.cell_size,
+                     self.cell_size)), width=1)
+                # Если клетка выбрана, то она закрашивается полностью
+
+                if self.board[j][i] == 1:  # X
+                    pygame.draw.line(screen, 'blue',
+                                     (self.left + self.cell_size * i + 2,
+                                      self.top + self.cell_size * j + 2),
+                                     (self.left + self.cell_size * (i + 1) - 2,
+                                      self.top + self.cell_size * (j + 1) - 2),
+                                     width=2
+                                     )
+                    pygame.draw.line(screen, 'blue',
+                                     (self.left + self.cell_size * (i + 1) - 2,
+                                      self.top + self.cell_size * j + 2),
+                                     (self.left + self.cell_size * i + 2,
+                                      self.top + self.cell_size * (j + 1) - 2),
+                                     width=2
+                                     )
+                if self.board[j][i] == 2:  # O
+                    pygame.draw.circle(screen, 'red',
+                                       (self.left + self.cell_size * i + self.cell_size // 2,
+                                        self.top + self.cell_size * j + self.cell_size // 2),
+                                       self.cell_size // 2 - 4,
+                                       width=2)
+
+    def get_cell(self, mouse_pos):
+        # Проверка наличия мыши в пределах поля
+        if (mouse_pos[0] < self.left or mouse_pos[0] > (self.left + self.width * self.cell_size) or
+                mouse_pos[1] < self.top or mouse_pos[1] > (self.top + self.height * self.cell_size)):
+            return None
+
+        # Находим столбец
+        cell = [0, 0]
+        for i in range(self.width):
+            if mouse_pos[0] < self.left + self.cell_size * (i + 1):
+                cell[1] = i
+                break
+
+        # Находим строку
+        for i in range(self.height):
+            if mouse_pos[1] < self.top + self.cell_size * (i + 1):
+                cell[0] = i
+                break
+
+        return tuple(cell)
+
+    def on_click(self, cell_coords):
+        global move
+        i, j = cell_coords
+        if self.board[i][j] == 0:
+            if move:  # X
+                self.board[i][j] = 1
+            else:
+                self.board[i][j] = 2
+            move = not move
+
+    def get_click(self, mouse_pos):
+        cell = self.get_cell(mouse_pos)
+        if cell:
+            self.on_click(cell)
+
+
+pygame.init()
+size = width, height = 500, 500
+screen = pygame.display.set_mode(size)
+
+pygame.display.set_caption('Реакция на события мыши')
+# CОздаём игровое поле
+board = Board(5, 7)
+board.set_view(100, 100, 50)
+
+# Запускаем игровой цикл
+running = True
+move = True  # 1 - X, 2 - O
+while running:
+    #  Обработка входящих событий
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            board.get_click(event.pos)
+
+    # Отрисовка
+    # Очищаем экран
+    screen.fill('black')
+    # Отрисовываем поле
+    board.render(screen)
+    # Обновлям экран
+    pygame.display.flip()
